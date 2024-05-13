64ef478c4e355d4608c8e78935b11c6b5bf30300
Joel Middleton <jl422083@dal.ca>
2024-05-13 20:22:22 -0300

    Added a text input box and basic styling to create the general layout of the app.

--- App.js
+++ App.js
@@ -1,18 +1,27 @@
-import { SafeAreaView, Text } from 'react-native';
+import { SafeAreaView, Text, TextInput } from 'react-native';
 import tw, { useDeviceContext } from 'twrnc';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import 'react-native-reanimated'; 
 
-function App() {
-  useDeviceContext(tw);
+import React, { useState } from 'react';
 
+function App() {
+
+  /* 
+  ** Created a starting point that is simply a text input the size of the device, 
+  ** that lets the user type on the app. Nothing is being saved/stored yet, 
+  ** but the user can type in the app, just to get a feel for how to setup 
+  ** the sizing and layout of the app.
+  */
   return (
     <Provider store={store}>
-      <SafeAreaView>
-        <Text style={tw`w-screen mt-16 text-center text-xl`}>
-          Your app code goes here.
-        </Text>
+      <SafeAreaView style={tw`flex-1 bg-yellow-100`}>
+      <TextInput
+        style={tw`max-w-full h-screen mt-4 text-left text-xl  border border-gray-500 p-3`}
+        multiline
+        numberOfLines={8}
+      />
       </SafeAreaView>
     </Provider>
   )
