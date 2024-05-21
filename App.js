2e0753914971543cd52431ddda441ead7350fc56
PerfectOctogon <blazecoc777@gmail.com>
2024-05-21 15:41:41 -0300

    Note objects can be edited and saved.

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import * as React from 'react';
 import { useState } from 'react';
-import { FlatList, Image, SafeAreaView, Text, TouchableOpacity, View } from 'react-native';
+import { FlatList, Image, SafeAreaView, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import 'react-native-reanimated';
 import { Provider } from 'react-redux';
 import tw, { useDeviceContext } from 'twrnc';
@@ -18,12 +18,11 @@ const Stack = createNativeStackNavigator();
 //This is the note object. Can be added dynamically
 const Note = ({item, nav}) => {
   return (
-    <View style = {[tw`w-1/3 aspect-square mb-1 mr-1 p-2 rounded-lg bg-[#2F0082]`]}>
-      <Text style={tw`text-white`}>{item.note}</Text>
-      <TouchableOpacity onPress={() => {nav.navigate('Details', {noteText : item.note});}}>
-        <Text style={tw`text-white font-semibold`}>see more...</Text>
-      </TouchableOpacity>
-    </View>
+    <TouchableOpacity onPress={() => {nav.navigate('Details', {note : item});}} style = {[tw`w-1/3 aspect-square mb-1 mr-1 p-2 rounded-lg bg-[#2F0082]`]}>
+      <View>
+        <Text style={tw`text-white`}>{item.note}</Text>
+      </View>
+    </TouchableOpacity>
   );
 }
 
@@ -36,9 +35,9 @@ function HomeScreen({navigation}){
     var note = {id : (data.length + 1).toString(), note : "New note created!!"};
     var newNoteArray = [...data, note]
     setExampleState(newNoteArray);
-    console.log(data);
     data.push(note);
   }
+  
   return(
     <View style={tw`w-full h-screen flex-1 bg-[#1c004f]`}>
       <FlatList
@@ -60,11 +59,20 @@ function HomeScreen({navigation}){
 }
 
 function DetailsScreen({route, navigation}){
-  console.log(route.noteText);
-  const {noteText} = route.params;
+  console.log(route.note);
+  const {note} = route.params;
+  const [text, setText] = useState('');
+  saveNote = () => {
+    console.log(text);
+    note.note = text;
+  }
+
   return(
-    <View style={tw`flex-1 items-center justify-center bg-[#2F0082]`}>
-      <Text style={tw`text-lg text-white`}>{noteText}</Text>
+    <View style={tw`flex-1 bg-[#2F0082] p-2`}>
+      <TextInput style={tw`text-lg text-white bg-[#3d00a9]`} multiline={true} onChangeText={text => setText(text)} placeholder='Type here'>{note.note}</TextInput>
+      <TouchableOpacity style={tw`items-center`} onPress={saveNote}>
+        <Text style={tw`font-bold text-white`}>SAVE NOTE</Text>
+      </TouchableOpacity>
     </View>
   );
 }
