8b16f781c8cc0e76cd2d8fac7f88c8bd21341cd7
PerfectOctogon <blazecoc777@gmail.com>
2024-05-21 13:30:51 -0300

    New note objects can be added dynamically.

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import * as React from 'react';
-import { FlatList, SafeAreaView, Text, TouchableOpacity, View } from 'react-native';
+import { useState } from 'react';
+import { FlatList, Image, SafeAreaView, Text, TouchableOpacity, View } from 'react-native';
 import 'react-native-reanimated';
 import { Provider } from 'react-redux';
 import tw, { useDeviceContext } from 'twrnc';
 import { store } from './store';
 
-
+//Daniel Flemming
 
 const generateData = (count) => Array.from({length : count}, (_, i) => ({id : (i + 1).toString(), note : "Note number " + (i+1).toString()}));
-const data = generateData(20);
+const data = generateData(0);
 
 const Stack = createNativeStackNavigator();
 
@@ -18,25 +19,42 @@ const Stack = createNativeStackNavigator();
 const Note = ({item, nav}) => {
   return (
     <View style = {[tw`w-1/3 aspect-square mb-1 mr-1 p-2 rounded-lg bg-[#2F0082]`]}>
-      <Text style={tw`text-white font-semibold`}>{item.note}</Text>
+      <Text style={tw`text-white`}>{item.note}</Text>
       <TouchableOpacity onPress={() => {nav.navigate('Details', {noteText : item.note});}}>
-        <Text>see more...</Text>
+        <Text style={tw`text-white font-semibold`}>see more...</Text>
       </TouchableOpacity>
     </View>
   );
 }
 
+
+
 function HomeScreen({navigation}){
+  const [exampleState, setExampleState] = useState(data);
+  const addNote = () => {
+    console.log("Tried adding note xd");
+    var note = {id : (data.length + 1).toString(), note : "New note created!!"};
+    var newNoteArray = [...data, note]
+    setExampleState(newNoteArray);
+    console.log(data);
+    data.push(note);
+  }
   return(
     <View style={tw`w-full h-screen flex-1 bg-[#1c004f]`}>
       <FlatList
         style={tw`w-full`}
-        data = {data}
+        data = {exampleState}
         keyExtractor = {(item) => item.id}
         renderItem = {({item}) => <Note item={item} nav={navigation}/>}
         numColumns = {3}
         contentContainerStyle={tw`p-4`}
       />
+      <TouchableOpacity style={tw`items-center justify-center pb-3`} onPress={addNote}>
+        <Image
+          source = {require('./assets/add.png')}
+          style={tw`w-10 h-10`}
+        />
+      </TouchableOpacity>
     </View>
   );
 }
@@ -45,8 +63,8 @@ function DetailsScreen({route, navigation}){
   console.log(route.noteText);
   const {noteText} = route.params;
   return(
-    <View style={tw`flex-1 items-center justify-center`}>
-      <Text style={tw`text-lg`}>{noteText}</Text>
+    <View style={tw`flex-1 items-center justify-center bg-[#2F0082]`}>
+      <Text style={tw`text-lg text-white`}>{noteText}</Text>
     </View>
   );
 }
@@ -59,7 +77,7 @@ function App() {
         <NavigationContainer>
           <Stack.Navigator screenOptions={{headerStyle: {backgroundColor : '#120033'}, headerTitleStyle: {color: 'white'} }} initialRouteName="Home">
             <Stack.Screen name="Home" component={HomeScreen}/>
-            <Stack.Screen name="Details" component={DetailsScreen}/>
+            <Stack.Screen name="Details" component={DetailsScreen} options={{ headerTintColor: '#ffffff'}}/>
           </Stack.Navigator>
         </NavigationContainer>
       </SafeAreaView>
