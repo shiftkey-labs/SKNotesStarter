2b827f0a7c4bc2a2f0cef3aff6f5b10bef93572c
cybardev <sheikh@cybar.dev>
2024-06-05 02:41:32 -0300

    feat: remove note if empty

--- components/NoteScreen.js
+++ components/NoteScreen.js
@@ -1,19 +1,31 @@
 import { useEffect, useState } from "react";
 import { View, TextInput } from "react-native";
 import tw from "twrnc";
-import { useUpdateNoteMutation } from "../db";
+import { useDeleteNoteMutation, useUpdateNoteMutation } from "../db";
 import DeleteButton from "./DeleteButton";
 
 function NoteScreen({ navigation, route }) {
   const [data, setData] = useState(route.params.data);
+  const [deleteNote] = useDeleteNoteMutation();
   const [updateNote] = useUpdateNoteMutation();
 
+  // update note in DB as it is modified
   useEffect(() => {
     updateNote(data);
     navigation.setOptions({
       headerRight: () => <DeleteButton navigation={navigation} note={data} />,
     });
   }, [data]);
+
+  // remove the note if it is empty when leaving edit screen
+  useEffect(() => {
+    navigation.addListener("beforeRemove", (_event) => {
+      updateNote(data);
+      if (data.title === "" && data.content === "") {
+        deleteNote(data);
+      }
+    });
+  }, [navigation]);
 
   return (
     <View
