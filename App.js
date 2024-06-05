a49fc63e11e7556e81ff415d872581f3761fc292
PerfectOctogon <blazecoc777@gmail.com>
2024-06-04 21:13:14 -0300

    Hotfix for title and proper text padding in menu

--- App.js
+++ App.js
@@ -11,24 +11,21 @@ import { store } from './store';
 
 //Daniel Flemming
 
-//const generateData = (count) => Array.from({length : count}, (_, i) => ({id : (i + 1).toString(), note : "Note number " + (i+1).toString()}));
-//const data = generateData(0);
-
 const Stack = createNativeStackNavigator();
-
-
 
 //This is the note object. Can be added dynamically
 const Note = ({item, nav}) => {
   return (
-    <TouchableOpacity onPress={() => {nav.navigate('Details', {note : item});}} style = {[tw`w-1/3 aspect-square mb-1 mr-1 p-2 rounded-lg bg-[#2F0082]`]}>
+    <TouchableOpacity onPress={() => {nav.navigate('Details', {note : item});}} style = {[tw`w-1/3 aspect-square mb-1 mr-1 p-5 rounded-lg bg-[#2F0082]`]}>
       <View>
-        <Text style={tw`text-white`}>{item.content}</Text>
+        <Text style={tw`text-white text-4 font-bold pb-1`}>{item.title.substring(0, 8) + "..."}</Text>
+        <Text style={tw`text-white`}>{item.content.substring(0,30) + "..."}</Text>
       </View>
     </TouchableOpacity>
   );
 }
 
+// HomeScreen component handles the main functionality of adding, searching, and displaying notes
 function HomeScreen({route, navigation}){
   const [addNote] = useAddNoteMutation();
   const {data, error, isLoading} = useFetchNotesQuery();
@@ -53,7 +50,7 @@ function HomeScreen({route, navigation}){
 
   return(
     <View style={tw`w-full h-screen flex-1 bg-[#1c004f]`}>
-      <View style={tw`items-center border-b-2 border-white pb-5`}>
+      <View style={tw`items-center pb-5`}>
         <Text style={tw`justify-center text-white font-bold pb-3 pt-5`}>Search/Quick add</Text>
         <TextInput style={tw`w-4/5 h-6 bg-[#2F0082] text-white`} placeholder='Type here...' onChangeText={(text) => {setText(text)}}></TextInput>
       </View>
@@ -75,9 +72,11 @@ function HomeScreen({route, navigation}){
   );
 }
 
+// DetailsScreen component handles the display, editing, and deletion of a single note
 function DetailsScreen({route, navigation}){
   const [updateNote] = useUpdateNoteMutation();
   const {note} = route.params;
+  const [title, setTitle] = useState(note.title);
   const [text, setText] = useState(note.content);
   const [deleteNote] = useDeleteNoteMutation();
   const deleteThisNote = () => {
@@ -85,8 +84,6 @@ function DetailsScreen({route, navigation}){
     navigation.popToTop();
   }
   React.useEffect(() => {
-    // Use `setOptions` to update the button that we previously specified
-    // Now the button includes an `onPress` handler to update the count
     navigation.setOptions({
       headerRight: () => 
       <TouchableOpacity style={tw`items-center justify-center pb-3`} onPress={deleteThisNote}>
@@ -99,13 +96,14 @@ function DetailsScreen({route, navigation}){
   }, [navigation]);
 
   saveNote = () => {
-    updateNote({id : note.id, content : text, title : " "});
+    updateNote({id : note.id, content : text, title : title});
     navigation.popToTop();
   }
 
 
   return(
     <ScrollView style={tw`flex-1 bg-[#2F0082] p-2 h-screen`} automaticallyAdjustKeyboardInsets={true}>
+      <TextInput style={tw`text-white bg-[#3d00a9]  w-full text-6 mb-5`} multiline={true} onChangeText={text => setTitle(text)} placeholder='Type here'>{note.title}</TextInput>
       <TextInput style={tw`text-white bg-[#3d00a9] w-full`} multiline={true} onChangeText={text => setText(text)} placeholder='Type here'>{note.content}</TextInput>
       <TouchableOpacity style={tw`items-center pt-5`} onPress={saveNote}>
         <Text style={tw`font-bold text-white`}>SAVE NOTE</Text>
